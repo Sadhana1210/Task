43417de845a0c6080eaca96afd11231fb26ecae1
Sadhana1210 <156051517+Sadhana1210@users.noreply.github.com>
2024-01-09 17:43:42 +0530

    Add files via upload

---
+++ Insert.java
@@ -0,0 +1,32 @@
+package com.example.demo.GitInsert;
+
+public class Insert {
+		String name,branch,clg;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getBranch() {
+			return branch;
+		}
+
+		public void setBranch(String branch) {
+			this.branch = branch;
+		}
+
+		public String getClg() {
+			return clg;
+		}
+
+		public void setClg(String clg) {
+			this.clg = clg;
+		}
+
+		}
+
+
